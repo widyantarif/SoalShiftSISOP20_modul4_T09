c821f1c2c83c079bcba83d853d6824baf9e04574
widyantarif <55181778+widyantarif@users.noreply.github.com>
2020-04-24 18:36:41 +0700

    Create ssfs.c

---
+++ ssfs.c
@@ -0,0 +1,13 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
